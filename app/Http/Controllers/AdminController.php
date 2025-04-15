4210e011655095075053071147ea373b3832a3ff
unknown <mdnaimul15902@gmail.com>
2025-04-15 11:03:03 +0400

    edit and delete brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\Image;
 use Intervention\Image\ImageManager;
@@ -46,6 +47,38 @@ class AdminController extends Controller
         $brand->image = $file_name;
         $brand->save();
         return redirect()->route('admin.brands')->with('status','Brand added successfully');
+    }
+
+    public function edit_brand($id)
+    {
+        $brand = Brand::findOrFail($id);
+        return view('admin.edit-brand',compact('brand'));
+    }
+
+
+    public function update_brand_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug,'.$request->id,
+            'image' => 'mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $brand = Brand::findOrFail($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if(File::exists(public_path('uploads/brands/').'/'.$brand->image)){
+                 File::delete(public_path('uploads/brands/').'/'.$brand->image);
+            }
+            $image = $request->file('image');
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp.'.'.$file_extention;
+            $this->GenerateBrandThumbnailIsImage($image,$file_name);
+            $brand->image = $file_name;
+        }
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status','Brand update successfully');
     }
 
     public function GenerateBrandThumbnailIsImage($image,$imageName)
--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,6 @@ Route::middleware(['auth',AuthAdmin::class])->group(function(){
     Route::get('/admin/brands',[AdminController::class, 'brands'])->name('admin.brands');
     Route::get('/admin/brand/add',[AdminController::class, 'add_brand'])->name('admin.add_brand');
     Route::post('/admin/brand/store',[AdminController::class, 'brand_store'])->name('admin.brand_store');
+    Route::get('/admin/brand/edit/{id}',[AdminController::class, 'edit_brand'])->name('admin.edit_brand');
+    Route::post('/admin/brand/update',[AdminController::class, 'update_brand_store'])->name('admin.brand.update');
 });
